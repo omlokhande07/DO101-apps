749b385da44276092e4255759561f05307f16bf3
om <lokhandeom189@gmail.com>
2025-01-27 14:49:03 +0530

    Updated contacts branch and amde changes

--- contacts/app.js
+++ contacts/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
+console.log("Contacts");
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
